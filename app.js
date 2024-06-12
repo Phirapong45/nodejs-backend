62131dd70778fa9dcc0b1a1f4bec96aa2fb19c7e
Phirapong <64160031@go.buu.ac.th>
2024-06-12 11:16:37 +0700

    first commit

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-// Import libraries ที่ใช้
+// Import libraries
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const http = require('http');
+const server = http.createServer();
 
-// Import route
-// const homeRoutes = require("./routes/home");
+//ใช้ dotenv ทำ environment variables
+require("dotenv").config({ path: "secret.env" });
+
+//import environment variables
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbOptions = process.env.DB_OPTIONS;
+
+//Import route
 const balanceRoutes = require("./routes/balance");
 const adminRoutes = require("./routes/admin");
 
-// สร้างแอปพลิเคชัน Express และใช้ bodyParser เพื่อแปลงข้อมูล JSON จากคำขอ
+//สร้างแอปพลิเคชัน Express และใช้ bodyParser เพื่อแปลงข้อมูล JSON จากคำขอ
 const app = express();
 app.use(bodyParser.json());
 
-// ตั้งค่า CORS
+//ตั้งค่า CORS
 const corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
 };
 app.use(cors(corsOptions));
 
-// path
+//path
 app.use("/balance", balanceRoutes);
 app.use("/admin", adminRoutes);
 
-// Middleware ที่จัดการ error
+//Middleware ที่จัดการ error
 app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
     const message = error.message;
     res.status(status).json({ message: message });
 });
 
-// เชื่อมต่อ MongoDB
+//เชื่อมต่อ MongoDB
+const mongoUri = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?${dbOptions}`;
+
+//เชื่อมต่อ MongoDB
 mongoose
-    .connect(
-        "mongodb+srv://phirapongintern:phirapong99@cluster0.op88gju.mongodb.net/wallet_db?retryWrites=true&w=majority&appName=Cluster0"
-    )
+    .connect(mongoUri)
     .then((result) => {
         // เช็คว่าต่อ database หรือยัง
         app.listen(8080, () => {
